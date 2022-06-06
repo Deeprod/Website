3ad492728e3a49521c9f41828541e8c57649b6b3
Deeprod <106252325+Deeprod@users.noreply.github.com>
2022-06-06 16:38:29 +1000

    first commit test on full folder

--- app-copyright.php
+++ app-copyright.php
@@ -3,7 +3,7 @@
 echo"<br>
 	<div class='appFooter'>
 		<div class='footer-title'>
-			Copyright © Jonathan Venturi 2020
+			Copyright © Jonathan Venturi 2020-2022 (git)
 		</div>
 		All Rights Reserved
 	</div>
